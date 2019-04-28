46c40175534fba86453b323c5a784bbdff82073c
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-04-28 16:43:54 +1000

    event save and delete stubs

--- app/src/main/java/com/github/zeroicq/plannerator/mvp/presenters/EventPresenter.kt
+++ app/src/main/java/com/github/zeroicq/plannerator/mvp/presenters/EventPresenter.kt
@@ -1,8 +1,11 @@
 package com.github.zeroicq.plannerator.mvp.presenters
 
+import android.util.Log
+import android.view.View
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.github.zeroicq.plannerator.PlanneratorApplication
+import com.github.zeroicq.plannerator.mvp.models.EventModel
 import com.github.zeroicq.plannerator.mvp.views.EventView
 import javax.inject.Inject
 
@@ -13,5 +16,13 @@ class EventPresenter: MvpPresenter<EventView>() {
     lateinit var app: PlanneratorApplication
     init {
         PlanneratorApplication.graph.inject(this)
+    }
+
+    fun onEditButtonClick(e: EventModel) {
+        Log.d(PlanneratorApplication.appName, "edit was clicked")
+    }
+
+    fun onDeleteButtonClick(e: EventModel) {
+        Log.d(PlanneratorApplication.appName, "delete was clicked")
     }
 }
--- app/src/main/java/com/github/zeroicq/plannerator/ui/fragments/EventFragment.kt
+++ app/src/main/java/com/github/zeroicq/plannerator/ui/fragments/EventFragment.kt
@@ -1,6 +1,7 @@
 package com.github.zeroicq.plannerator.ui.fragments
 
 import android.databinding.DataBindingUtil
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,12 @@ import com.github.zeroicq.plannerator.databinding.FragmentEventBinding
 import com.github.zeroicq.plannerator.mvp.models.EventModel
 import com.github.zeroicq.plannerator.mvp.presenters.EventPresenter
 import com.github.zeroicq.plannerator.mvp.views.EventView
-import com.github.zeroicq.plannerator.util.getDayWithMonthWithYearString
 
 class EventFragment : BaseFragment(), EventView {
     enum class BUNDLE_KEYS {EVENT}
 
     private lateinit var binding: FragmentEventBinding
-    //
+
     @InjectPresenter
     lateinit var presenter: EventPresenter
 
@@ -25,11 +25,18 @@ class EventFragment : BaseFragment(), EventView {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_event, container, false)
 
-        val event = arguments!!.getParcelable<EventModel>(BUNDLE_KEYS.EVENT.toString())
-        //todo: add start/end date
-        binding.dateText.text = getDayWithMonthWithYearString(context!!, event.startDate)
+        val event = arguments!!.getParcelable<EventModel>(BUNDLE_KEYS.EVENT.toString())!!
+
+        binding.editButton.setOnClickListener { presenter.onEditButtonClick(event) }
+        binding.deleteButton.setOnClickListener { presenter.onDeleteButtonClick(event) }
+
+        val timeFormat = SimpleDateFormat("HH:mm")
+        val startTime = timeFormat.format(event.startDate)
+        val endTime = timeFormat.format(event.endDate)
+
+        binding.dateText.text = "$startTime - $endTime"
         binding.eventTitle.text = event.title
         binding.eventMessage.text = event.message
-        return  binding.root
+        return binding.root
     }
 }
